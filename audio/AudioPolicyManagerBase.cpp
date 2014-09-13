21b29c55506ed59b069011a49596055f5fac39d5
LorDClockaN <davor@losinj.com>
2014-09-13 21:10:46 +0200

    Fix build for Tegra board after CM-11.0 branch merge
    Change-Id: If0f85f5d86db60dbd6c7bf7fda704d92805b3c49

--- audio/AudioPolicyManagerBase.cpp
+++ audio/AudioPolicyManagerBase.cpp
@@ -1489,6 +1489,7 @@ bool AudioPolicyManagerBase::isOffloadSupported(const audio_offload_info_t& offl
         return false;
     }
 
+#ifndef ($(TARGET_BOARD_PLATFORM),tegra)
     char propValue[PROPERTY_VALUE_MAX];
     bool pcmOffload = false;
     if (audio_is_offload_pcm(offloadInfo.format)) {
@@ -1504,6 +1505,7 @@ bool AudioPolicyManagerBase::isOffloadSupported(const audio_offload_info_t& offl
             return false;
         }
     }
+#endif
 
     // Check if offload has been disabled
     if (property_get("audio.offload.disable", propValue, "0")) {
@@ -3821,8 +3823,10 @@ const struct StringToEnum sFormatNameToEnumTable[] = {
     STRING_TO_ENUM(AUDIO_FORMAT_EVRCNW),
     STRING_TO_ENUM(AUDIO_FORMAT_FLAC),
 #endif
+#ifndef ($(TARGET_BOARD_PLATFORM),tegra)
     STRING_TO_ENUM(AUDIO_FORMAT_PCM_16_BIT_OFFLOAD),
     STRING_TO_ENUM(AUDIO_FORMAT_PCM_24_BIT_OFFLOAD),
+#endif
 };
 
 const struct StringToEnum sOutChannelsNameToEnumTable[] = {
